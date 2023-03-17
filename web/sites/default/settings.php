80e5ed229b2ac93ce6b617c1e559f33ce51ce872
Marcin Grabias <mgrabias@ogtrading.eu>
2023-03-17 07:39:16 +0100

    Site settings updates.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -759,22 +759,21 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 // Automatically generated include for settings managed by ddev.
 if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include __DIR__ . '/settings.ddev.php';
-}
 
-/**
- * Load local development override configuration, if available.
- *
- * Create a settings.local.php file to override variables on secondary (staging,
- * development, etc.) installations of this site.
- *
- * Typical uses of settings.local.php include:
- * - Disabling caching.
- * - Disabling JavaScript/CSS compression.
- * - Rerouting outgoing emails.
- *
- * Keep this code block at the end of this file to take full effect.
- */
-
-if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-  include $app_root . '/' . $site_path . '/settings.local.php';
+  /**
+   * Load local development override configuration, if available.
+   *
+   * Create a settings.local.php file to override variables on secondary (staging,
+   * development, etc.) installations of this site.
+   *
+   * Typical uses of settings.local.php include:
+   * - Disabling caching.
+   * - Disabling JavaScript/CSS compression.
+   * - Rerouting outgoing emails.
+   *
+   * Keep this code block at the end of this file to take full effect.
+   */
+  if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+    include $app_root . '/' . $site_path . '/settings.local.php';
+  }
 }
