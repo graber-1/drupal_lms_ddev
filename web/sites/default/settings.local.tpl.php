325033e499144c01d07bac8a7a4716f8d391fde3
Marcin Grabias <mgrabias@ogtrading.eu>
2024-06-12 12:08:20 +0200

    More config updates.

--- web/sites/default/settings.local.tpl.php
+++ web/sites/default/settings.local.tpl.php
@@ -7,3 +7,13 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 $config['system.logging']['error_level'] = 'verbose';
+
+/**
+ * Ensure that development modules are not being exported into configuration.
+ */
+$settings['config_exclude_modules'] = [
+  'reroute_email',
+  'stage_file_proxy',
+  'config_inspector',
+  'developer_console',
+];
