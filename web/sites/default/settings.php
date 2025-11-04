9aa6810d255978b62e8e35c91e0bcb0d471fc1ad
Marcin Grabias <marcin@tag1consulting.com>
2025-11-04 16:07:03 +0100

    Configured private file system.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -522,7 +522,7 @@ $settings['update_free_access'] = FALSE;
  * See https://www.drupal.org/documentation/modules/file for more information
  * about securing private files.
  */
-# $settings['file_private_path'] = '';
+$settings['file_private_path'] = '../private_files';
 
 /**
  * Temporary file path:
